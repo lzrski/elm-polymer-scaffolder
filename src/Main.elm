c26120a4952c9e4087525a9694c5d331b251d8d3
Tadeusz Łazurski <tadeusz@lazurski.pl>
2017-07-02 19:19:38 +0200

    Implement basic fetching of analyzer data
    Not decoded yet.

--- src/Main.elm
+++ src/Main.elm
@@ -1,12 +1,15 @@
 module Main exposing (main)
 
-import Html exposing (Html, pre, text)
+import Html exposing (Html, div, pre, text)
+import Html.Attributes exposing (style)
+import Http
+import Json.Decode as Decode exposing (value)
 
 
 main : Program Never Model Msg
 main =
     Html.program
-        { init = state ! []
+        { init = init
         , update = update
         , subscriptions = subscriptions
         , view = view
@@ -14,16 +17,40 @@ main =
 
 
 type alias Model =
-    ()
+    { analysis : Maybe Analysis
+    , error : Maybe Http.Error
+    }
+
+
+type alias Analysis =
+    Decode.Value
 
 
 type Msg
     = NoOp
+    | DataFetched (Result Http.Error Analysis)
 
 
-state : Model
-state =
-    ()
+init : ( Model, Cmd Msg )
+init =
+    { analysis = Nothing
+    , error = Nothing
+    }
+        ! [ fetchData "paper-icon-button" ]
+
+
+fetchData component =
+    let
+        url =
+            "https://www.webcomponents.org/api/docs/PolymerElements/"
+                ++ component
+                ++ "?use_analyzer_data"
+
+        _ =
+            Debug.log "Getting analysis" url
+    in
+        Http.get url Decode.value
+            |> Http.send DataFetched
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -32,12 +59,56 @@ update msg state =
         NoOp ->
             state ! []
 
+        DataFetched (Ok value) ->
+            let
+                _ =
+                    Debug.log "Data fetch OK" value
+            in
+                { state | analysis = Just value } ! []
+
+        DataFetched (Err message) ->
+            let
+                _ =
+                    Debug.log "Error fetching data" message
+            in
+                { state | error = Just message } ! []
+
 
 subscriptions : Model -> Sub Msg
 subscriptions state =
     Sub.none
 
 
+
+-- VIEW
+
+
 view : Model -> Html Msg
 view model =
-    pre [] [ text "Let's get started!" ]
+    div []
+        [ errorBox model.error
+        , codeBox model.analysis
+        ]
+
+
+codeBox analysis =
+    case analysis of
+        Nothing ->
+            pre [] [ text "Loading analyzer data..." ]
+
+        Just data ->
+            div [] [ text (toString data) ]
+
+
+errorBox error =
+    case error of
+        Nothing ->
+            div [] []
+
+        Just error ->
+            pre
+                [ style
+                    [ ( "background", "red" )
+                    ]
+                ]
+                [ text (toString error) ]
