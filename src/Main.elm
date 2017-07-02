7788d4e1780a4b459b683814293fdcff247662a0
Tadeusz Łazurski <tadeusz@lazurski.pl>
2017-07-02 17:40:31 +0200

    Upgrade to Html.program

--- src/Main.elm
+++ src/Main.elm
@@ -3,6 +3,41 @@ module Main exposing (main)
 import Html exposing (Html, pre, text)
 
 
-main : Html msg
+main : Program Never Model Msg
 main =
+    Html.program
+        { init = state ! []
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+type alias Model =
+    ()
+
+
+type Msg
+    = NoOp
+
+
+state : Model
+state =
+    ()
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg state =
+    case msg of
+        NoOp ->
+            state ! []
+
+
+subscriptions : Model -> Sub Msg
+subscriptions state =
+    Sub.none
+
+
+view : Model -> Html Msg
+view model =
     pre [] [ text "Let's get started!" ]
